2f54ed58602c4e6bccbd13192b3381714e26a5d4
Steven Adams <steven.adams.r@gmail.com>
2024-05-17 18:04:00 -0400

    Removed fade animation

--- src/lib/components/grid-item-guessed.svelte
+++ src/lib/components/grid-item-guessed.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
-
 	export let character: string;
 	export let background: string;
 </script>
 
-<div transition:fade class={`w-full h-full flex justify-center items-center rounded ${background}`}>
+<div class={`w-full h-full flex justify-center items-center rounded ${background}`}>
 	{character}
 </div>
